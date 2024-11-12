33772cc538d910cd30f51ab11c64391f66c6cc37
H-E1P <xe1f.epii@gmail.com>
2024-11-12 20:36:42 +0900

    .

--- nuxpersonalheader.h
+++ nuxpersonalheader.h
@@ -2,6 +2,13 @@
 #ifndef _NUX_PERSONAL_HEADER
 # define _NUX_PERSONAL_HEADER
 
+/**
+*
+* # How2Install
+* can install by `wget http://h-e1p.github.io/nuxpersonalheader.h`
+*
+*/
+
 #include <stdio.h>
 //#define IL inline
 //#define CLOSES(X) X)
